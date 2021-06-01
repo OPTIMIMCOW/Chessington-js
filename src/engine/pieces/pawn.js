46ff9cf98bace9c186e96f089b9514dc9f361198
Ken Allan <Ken.Allan@softwire.com>
2021-06-01 14:16:36 +0100

    made pawns move a bit better

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -5,34 +5,28 @@ import Piece from './piece';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
-        this.firstMove=true;
     }
 
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
 
-        if(this.firstMove===true){
-
-            this.firstMove = false;
-
-            if (this.player === Player.WHITE) {
+        if (this.player === Player.WHITE) {
+            if (location.row === 1) {
                 return [Square.at(location.row + 2, location.col), Square.at(location.row + 1, location.col)]
+
             } else {
+                return [Square.at(location.row + 1, location.col)]
+            }
+        } else {
+            if (location.row === 6) {
                 return [Square.at(location.row - 2, location.col), Square.at(location.row - 1, location.col)]
-            }
-            
 
-        }else{
-
-            if (this.player === Player.WHITE) {
-                return Square.at(location.row + 1, location.col)
             } else {
-                return Square.at(location.row - 1, location.col)
+                return [Square.at(location.row - 1, location.col)]
             }
-
         }
- 
+
     }
 
 
