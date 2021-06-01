2a2c2512c6be15ac3169d0dca8870f9f947a39ba
Francesca Stocco <Francesca.Stocco@softwire.com>
2021-06-01 13:49:10 +0100

    Part 1 Test 1

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -5,14 +5,35 @@ import Piece from './piece';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
+        this.firstMove=true;
     }
+
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+
+        if(this.firstMove===true){
+
+            this.firstMove = false;
+
+            if (this.player === Player.WHITE) {
+                return [Square.at(location.row + 2, location.col), Square.at(location.row + 1, location.col)]
+            } else {
+                return [Square.at(location.row - 2, location.col), Square.at(location.row - 1, location.col)]
+            }
+            
+
+        }else{
+
+            if (this.player === Player.WHITE) {
+                return Square.at(location.row + 1, location.col)
+            } else {
+                return Square.at(location.row - 1, location.col)
+            }
+
         }
+ 
     }
+
+
 }
