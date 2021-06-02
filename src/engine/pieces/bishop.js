27b1d7f2d4a6ec563bcefe009c326767c35a6ac9
Ken Allan <Ken.Allan@softwire.com>
2021-06-02 15:17:51 +0100

    cherrypick 10 added obstacle to bishop

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -9,14 +9,38 @@ export default class Bishop extends Piece {
     getAvailableMoves(board) {
         const location = board.findPiece(this);
         const moves = [];
+        let tempSquare;
+        let go1 = true;
+        let go2 = true;
+        let go3 = true;
+        let go4 = true;
         
-        for (let i = 1; i < 8; i ++){
-            moves.push(new Square(location.row + i, location.col + i));
-            moves.push(new Square(location.row + i, location.col - i));
-            moves.push(new Square(location.row - i, location.col - i));
-            moves.push(new Square(location.row - i, location.col + i));
+        for (let i = 1; i < 8; i ++){  
+            tempSquare = new Square(location.row + i, location.col + i)
+            if (!board.getPiece(tempSquare) && go1) {
+                moves.push(tempSquare);
+            } else {
+                go1 = false;
+            }
+            tempSquare = new Square(location.row + i, location.col - i)
+            if (!board.getPiece(tempSquare) && go2) {
+                moves.push(tempSquare);
+            } else {
+                go2 = false;
+            }
+            tempSquare = new Square(location.row - i, location.col - i)
+            if (!board.getPiece(tempSquare) && go3) {
+                moves.push(tempSquare);
+            } else {
+                go3 = false;
+            }
+            tempSquare = new Square(location.row - i, location.col + i)
+            if (!board.getPiece(tempSquare) && go4) {
+                moves.push(tempSquare);
+            } else {
+                go4 = false;
+            }
         }
-
         const legalMoves = board.filterMoves(moves);
         return legalMoves;
     }
