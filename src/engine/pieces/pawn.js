49b79d493561c74ae915a24a9a1f09c975eb6c7b
Ken Allan <Ken.Allan@softwire.com>
2021-06-03 12:26:39 +0100

    cherry 17 implemented pawn kill moves

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,42 +1,55 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+import King from './king';
 
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
 
+    killPawnMove(row, col, location, moves, board) {
+        const tempSquare = new Square(location.row + row, location.col + col);
+        if (board.onBoard(tempSquare) && board.getPiece(tempSquare) && board.getPiece(tempSquare).player !== this.player && !(board.getPiece(tempSquare) instanceof King)) {
+            moves.push(tempSquare);
+        }
+    }
+
+    runPawnMove(rowVector, location, moves, board) {
+
+        const tempSquare = new Square(location.row + rowVector, location.col);
+        if (board.onBoard(tempSquare)) {
+            const getPiece = board.getPiece(tempSquare);
+            if (!getPiece) {
+                moves.push(tempSquare);
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
+
     getAvailableMoves(board) {
         const location = board.findPiece(this);
         const moves = [];
-        let tempSquare;
 
         if (this.player === Player.WHITE) {
-            tempSquare = new Square(location.row + 1, location.col);
-            if (board.onBoard(tempSquare) && !board.getPiece(tempSquare)) {
-                moves.push(tempSquare);
-                if (location.row === 1) {
-                    tempSquare = new Square(location.row + 2, location.col);
-                    if (!(board.getPiece(tempSquare))) {
-                        moves.push(tempSquare);
-                    }
-                }
-
+            const firstSquareEmpty = this.runPawnMove(1, location, moves, board);
+            if (firstSquareEmpty && location.row === 1) {
+                this.runPawnMove(2, location, moves, board);
             }
+            this.killPawnMove(1, 1, location, moves, board);
+            this.killPawnMove(1, -1, location, moves, board);
         } else {
-            tempSquare = new Square(location.row - 1, location.col);
-            if (board.onBoard(tempSquare) && !board.getPiece(tempSquare)) {
-                moves.push(tempSquare);
-                if (location.row === 6) {
-                    tempSquare = new Square(location.row - 2, location.col);
-                    if (!(board.getPiece(tempSquare))) {
-                        moves.push(tempSquare);
-                    }
-                }
+            const firstSquareEmpty = this.runPawnMove(-1, location, moves, board);
+            if (firstSquareEmpty && location.row === 6) {
+                this.runPawnMove(-2, location, moves, board);
             }
+            this.killPawnMove(-1, 1, location, moves, board);
+            this.killPawnMove(-1, -1, location, moves, board);
         }
-        const legalMoves = board.filterMovesOnBoard(moves);
-        return legalMoves;
+        console.log(moves);
+        return moves;
     }
 }
